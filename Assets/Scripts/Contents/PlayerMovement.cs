6fb56722bee7e84e29063be2be49b44d99335b02
weinhooney <devweinhooney@gmail.com>
2024-06-19 16:12:23 +0900

    점프, 2단 점프 구현

--- Assets/Scripts/Contents/PlayerMovement.cs
+++ Assets/Scripts/Contents/PlayerMovement.cs
@@ -1,9 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float _speed = 5.0f; // 좌우 이동 속도
+    [SerializeField] private float _jumpForce = 8.0f; // 점프 힘
+    [SerializeField] private LayerMask _groundLayer; // 바닥체크를 위한 레이어
+    [SerializeField] private int _maxJumpCount = 2;		// 땅을 밟기 전까지 할 수 있는 최대 점프 횟수
+    [SerializeField] private float _normalGravityScale = 2.5f;
+    
+    private Rigidbody2D _rigid;
+    private CapsuleCollider2D _collider;
+
+    private bool _isGrounded; // 바닥체크
+    private Vector3 _footPos; // 발 위치
+    private	int	_currentJumpCount = 0;	// 현재 가능한 점프 횟수
+    
     public bool IsLongJump { get; set; } = false;
 
     public void Move(float x)
@@ -13,6 +27,46 @@ public class PlayerMovement : MonoBehaviour
 
     public void Jump()
     {
+        if (0 < _currentJumpCount)
+        {
+            _rigid.velocity = Vector2.up * _jumpForce;
+            _currentJumpCount--;
+        }
+    }
+    
+    private void Awake()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<CapsuleCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // 바닥체크
+        Bounds bounds = _collider.bounds;
+        _footPos = new Vector2(bounds.center.x, bounds.min.y);
+        _isGrounded = Physics2D.OverlapCircle(_footPos, 0.1f, _groundLayer);
         
+        // 플레이어의 발이 땅에 닿아있고, y축 속도가 0이하이면 점프횟수 초기화
+        if (_isGrounded && _rigid.velocity.y <= 0)
+        {
+            _currentJumpCount = _maxJumpCount;
+        }
+        
+        // 점프 높이 조절
+        if (IsLongJump && 0 < _rigid.velocity.y)
+        {
+            _rigid.gravityScale = 1.0f;
+        }
+        else
+        {
+            _rigid.gravityScale = _normalGravityScale;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(_footPos, 0.1f);
     }
 }
